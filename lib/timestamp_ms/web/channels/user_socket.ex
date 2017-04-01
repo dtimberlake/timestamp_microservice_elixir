3693e0b1c3c35d61024c7048a2c4a600af49e2a0
dtimberlake <daniel@danieltimberlake.com>
2017-04-01 17:04:41 -0500

    added heroku deploy capabilities

--- lib/timestamp_ms/web/channels/user_socket.ex
+++ lib/timestamp_ms/web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule TimestampMs.Web.UserSocket do
   # channel "room:*", TimestampMs.Web.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
